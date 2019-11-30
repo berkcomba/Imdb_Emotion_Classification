43d4951efcf2ad2b654235b76555b844a56f68b9
Riza Berk Comba <57064296+berkcomba@users.noreply.github.com>
2019-12-01 01:59:54 +0300

    Update Imdb.py

--- Imdb.py
+++ Imdb.py
@@ -36,7 +36,8 @@ from keras import layers
 
 model = models.Sequential()
 model.add(layers.Dense(16,activation="relu",input_shape=(10000,)))
-model.add(layers.Dense(16,activation="relu"))
+#model.add(layers.Dense(16,activation="relu"))
+#model.add(layers.Dense(16,activation="relu"))
 model.add(layers.Dense(1,activation="sigmoid"))
 model.compile(optimizer="rmsprop",
               loss="binary_crossentropy",
